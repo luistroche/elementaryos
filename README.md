9da789a6031f6d236961de5027f672584715c74e
Luis Troche <luistroche@gmail.com>
2014-05-24 18:33:23 -0400

    README EDIT, adding ver to script, prototipe funtions. Not yet tested

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-elementaryos
+Elementary-os
 ============
 
-Steps to do after installing elementaryos
+La idea es crear un pequeno script, el cual tiene la funcion de automatizar todos los pasos que regularmente siguen la instalacion de ElementaryOs.
+
+El porque hago esto?. Dos motivos
+Elementaryos es un sistema operativo el cual tiene mi agrado.Me interesaria instalar el sistema operativo en mis computadoras de produccion (desarrollo web), pero tiene la tendencia a desconfigurarse muy facilmente (apt-get and essential files). Tengo que efectuar varios tests para poder confiar plenamente en el sistema operativo.
+El segundo motivo es la practica de escribir codigo Python para Bash.
+Para lograr mi cometido utilizo virtualbox con clones del sistema operativo.
--- elementary-after-install-python-script.py
+++ elementary-after-install-python-script.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
+#ver 0.1 
+#non-classes
+#simple variables and funtions.
 import sys
 import subprocess
 sud = "sudo"
 aptget = "apt-get"
-installer = [sud, aptget, "check"]
+aptadd = "apt-add-repository"
+aptkey = ["apt-key", "adv", "--keyserver", "keyserver.ubuntu.com", "--recv-keys"]
+installer = [sud, aptget,]
 needed_repositories = ["ppa:webupd8team/java", "ppa:pipelight/stable", "deb http://repository.spotify.com stable non-free"]
 needed_repositories_keys = ["94558F59"]
 packages = ["git", "gedit", "ssh", "linux-image-generic-lts-sausy", "xserver-xorg-lts-saucy", "libgl1-mesa-glx-lts-saucy", "gufw", "oracle-java7-installer", "libreoffice", "libxss1", "dconf-tools", "hardinfo", "pavucontrol", "spotify-client", "palimpsest"]
+checker = True
+def repository(repo):
+	return subprocess.call(aptadd, "-y", repo)
 
+def apt_get(package):
+	return subprocess.call(installer, "-y", package)
 
-subprocess.call(installer)
+if checker:
+	for i in needed_repositories:
+		repository(i)
+
+#subprocess.call()
