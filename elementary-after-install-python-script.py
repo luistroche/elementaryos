f6118537bde727e2075ce9aa80ffca6703cf1015
Luis Troche <luistroche@gmail.com>
2014-05-24 09:43:41 -0400

    adding some packages to the script.

--- elementary-after-install-python-script.py
+++ elementary-after-install-python-script.py
@@ -4,7 +4,9 @@ import subprocess
 sud = "sudo"
 aptget = "apt-get"
 installer = [sud, aptget, "check"]
-packages = []
+needed_repositories = ["ppa:webupd8team/java", "ppa:pipelight/stable", "deb http://repository.spotify.com stable non-free"]
+needed_repositories_keys = ["94558F59"]
+packages = ["git", "gedit", "ssh", "linux-image-generic-lts-sausy", "xserver-xorg-lts-saucy", "libgl1-mesa-glx-lts-saucy", "gufw", "oracle-java7-installer", "libreoffice", "libxss1", "dconf-tools", "hardinfo", "pavucontrol", "spotify-client", "palimpsest"]
 
 
 subprocess.call(installer)
