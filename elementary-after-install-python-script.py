78c978155f63fb3e1a60c7da92a2a9f4d753d6d3
Luis Troche <luistroche@gmail.com>
2014-05-25 11:55:51 -0400

    script adding repositories

--- elementary-after-install-python-script.py
+++ elementary-after-install-python-script.py
@@ -8,19 +8,23 @@ sud = "sudo"
 aptget = "apt-get"
 aptadd = "apt-add-repository"
 aptkey = ["apt-key", "adv", "--keyserver", "keyserver.ubuntu.com", "--recv-keys"]
-installer = [sud, aptget,]
-needed_repositories = ["ppa:webupd8team/java", "ppa:pipelight/stable", "deb http://repository.spotify.com stable non-free"]
+installer = [sud, aptget,"-y"]
+needed_repositories = ["ppa:webupd8team/java","ppa:pipelight/stable", "deb http://repository.spotify.com, stable non-free"]
+
 needed_repositories_keys = ["94558F59"]
-packages = ["git", "gedit", "ssh", "linux-image-generic-lts-sausy", "xserver-xorg-lts-saucy", "libgl1-mesa-glx-lts-saucy", "gufw", "oracle-java7-installer", "libreoffice", "libxss1", "dconf-tools", "hardinfo", "pavucontrol", "spotify-client", "palimpsest"]
+
+system_packages = ["linux-image-generic-lts-sausy", "xserver-xorg-lts-saucy", "libgl1-mesa-glx-lts-saucy"]
+regular_packages = ["git", "gedit", "ssh", "gufw", "oracle-java7-installer", "libreoffice", "libxss1", "dconf-tools", "hardinfo", "pavucontrol", "spotify-client", "palimpsest"]
+
 checker = True
+
 def repository(repo):
 	return subprocess.call(aptadd, "-y", repo)
 
 def apt_get(package):
 	return subprocess.call(installer, "-y", package)
 
-if checker:
-	for i in needed_repositories:
+for i in needed_repositories:
 		repository(i)
 
 #subprocess.call()
